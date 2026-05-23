a7596cfaef8303a61b80c8d8ebe55945e2d5d376
Hugo Silva <developer.hugosilva@gmail.com>
2026-05-23 20:05:59 -0300

    Commit da versão do Claude

--- Codigos/jogo_da_velha.py
+++ Codigos/jogo_da_velha.py
@@ -1,117 +1,93 @@
-
-
 tabuleiro = [
-    [' ',  ' ', ' '],
-    [' ',  ' ', ' '],
-    [' ',  ' ', ' '],
-
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
 ]
-#print(tabuleiro)
 
 jogador = 'X'
 
+
 def temGanhador():
-    #Revificando as linhas
-    for linhas in range(3):
-        if(
-        tabuleiro[linha][0] != ' ' and    
-        tabuleiro[linha][0] == tabuleiro[linha][1] and
-        tabuleiro[linha][0] == tabuleiro[linha][2]
-        ):
-            print(f'{tabuleiro[linha][0]} GANHOU!!!')
-            return True
+    # Verificar linhas
+    for linha in range(3):
+        if (tabuleiro[linha][0] != ' ' and
+                tabuleiro[linha][0] == tabuleiro[linha][1] == tabuleiro[linha][2]):
+            return tabuleiro[linha][0]
 
-    #Revificando as linhas
-        for linhas in range(3):
-            if(
-            tabuleiro[0][coluna] != ' ' and    
-            tabuleiro[0][coluna] == tabuleiro[1][coluna] and
-            tabuleiro[0][coluna] == tabuleiro[2][coluna]
-            ):
-                print(f'{tabuleiro[0][coluna]} GANHOU!!!')
-                return True
+    # Verificar colunas
+    for coluna in range(3):
+        if (tabuleiro[0][coluna] != ' ' and
+                tabuleiro[0][coluna] == tabuleiro[1][coluna] == tabuleiro[2][coluna]):
+            return tabuleiro[0][coluna]
 
-    #Verificar diagonais
-    if (
-        tabuleiro[1][1] != ' ' and
-        (
-            (
-                tabuleiro[0][0] == tabuleiro[1][1] and 
-                tabuleiro[0][0] == tabuleiro[2][2]
-            ) or 
-            (
-                tabuleiro[0][2] == tabuleiro[1][1] and 
-                tabuleiro[1][1] == tabuleiro[2][0])
-        )
-    ):
-        print(f'{tabuleiro[1][1]} GANHOU!!!')
-        return True
+    # Verificar diagonais
+    if (tabuleiro[1][1] != ' ' and (
+        tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] or
+        tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0]
+    )):
+        return tabuleiro[1][1]
 
-    #Se não teve ganhador - Ai retorna salfo        
-    return False
+    return None
+
 
 def temEmpate():
-
     for linha in range(3):
         for coluna in range(3):
-            for coluna in range(3):
-                if tabuleiro[linha][coluna] == ' ':
-                    return False
-    print('GALOU')
+            if tabuleiro[linha][coluna] == ' ':
+                return False
     return True
 
+
 def exibeTab():
-    for linha in tabuleiro:
-        print(' | '.join(linha)) # Join é um método - Junta coisas de uma lista com o separador escolhido
-        #print(f'{linha[0]} | {linha[1]} | {linha[2]}')
-        print('-'*10)
+    print()
+    print('       0   1   2')
+    print('     +---+---+---+')
+    for i, linha in enumerate(tabuleiro):
+        print(f'  {i}  | {" | ".join(linha)} |')
+        print('     +---+---+---+')
+    print()
+
 
 def jogada(linha, coluna):
-    if (not 0 <= linha <= 2 or 
-        not 0 <= coluna <= 2 or
-        #Se linha ou coluna não fazem parte desse Range 
-        tabuleiro[linha][coluna] != ' ' #Se não estiver vazio
-    ):
-        print('Jogada Invalida')
-        return jogador #Retorna o jogador atual
-    #Acessar váriavel global
-    #global jogador # Não recomendavel - Ficar dentro de uma função alterando valor de uma váriavel global
-    #Melhor fazer um return
+    if (not 0 <= linha <= 2 or
+            not 0 <= coluna <= 2 or
+            tabuleiro[linha][coluna] != ' '):
+        return None
     tabuleiro[linha][coluna] = jogador
-    '''
-    if jogador == 'X':
-        return 'O'
-    else:
-        return 'X'
-    '''
-    return 'O' if jogador == 'X' else 'X' #Retorna O se jogador já for X, se ele for O return X ] - Retorna outro jogador
+    return 'O' if jogador == 'X' else 'X'
 
-'''
-jogador = jogada(1, 1)
-jogador = jogada(1, 2)
-jogador = jogada(1, 2)
-'''
 
-while True:
-    print(f'Jogador da vez: {jogador}')
-    try: 
-        linha = int(input('Digite a linha: '))
-        coluna = int(input('Digite a coluna: '))
-        jogador = jogada(linha, coluna)
-    except (ValueError, IndexError): #Podendo tratar os erros de forma separada - Para mostar mensagens mais coerentes para cada exesão  
-        print('Digite valores numéricos entre 0 e 2') #Não quebra e trata o erro 
-
-    if temGanhador() or temEmpate():
-        break
-    
-    exibeTab()
+print()
+print('  =========================')
+print('      J O G O  D A  V E L H A')
+print('  =========================')
+print('  Linha e coluna: valores de 0 a 2')
 exibeTab()
 
-#Outro método
-'''
-tabuleiro2 = [ [' ' for _ in range(0, 3)] for _ in range(0, 3)] # undeline (_) - Usar e descartar
-print(tabuleiro2)
-'''
+while True:
+    print(f'  Jogador da vez: [ {jogador} ]')
+    try:
+        linha = int(input('    Linha  (0-2): '))
+        coluna = int(input('    Coluna (0-2): '))
+    except ValueError:
+        print('  Entrada invalida! Digite apenas numeros.\n')
+        continue
 
-#if ternario
-#Tratamento de exceções 
+    proximo = jogada(linha, coluna)
+    if proximo is None:
+        print('  Jogada invalida! Posicao fora do limite ou ja ocupada.\n')
+        continue
+
+    jogador = proximo
+    exibeTab()
+
+    ganhador = temGanhador()
+    if ganhador:
+        print(f'  Parabens! Jogador [ {ganhador} ] GANHOU!')
+        print()
+        break
+
+    if temEmpate():
+        print('  EMPATE! Foi um bom jogo!')
+        print()
+        break
